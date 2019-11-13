cc6ad3e4081c10525413bbab2901b9c62c2bf847
Đào Minh Toàn <v.toandm2@vinid.net>
2019-11-13 15:02:50 +0700

    update fcn Model

--- models/model.py
+++ models/model.py
@@ -4,7 +4,7 @@ import torch
 from modules import Unet
 
 
-class Model(nn.Module):
+class deeplapModel(nn.Module):
     def __init__(self, num_class=4):
         super(Model, self).__init__()
         self.models = torchvision.models.segmentation.deeplabv3_resnet50(pretrained=False, progress=True, num_classes=num_class, aux_loss=None)
@@ -14,7 +14,15 @@ class Model(nn.Module):
         out = self.models(inputs)['out']
         return out
 
-# if __name__=='__main__':
+class fcnModel(nn.Module):
+    def __init__(self, num_class=4):
+        super(fcnModel, self).__init__()
+        self.models = torchvision.models.segmentation.fcn_resnet50(pretrained=True, num_classes=num_class)
+    def forward(self, inputs):
+        out = self.models(inputs)
+        return out
+
+# if.segmentation.fcn_resnet50__name__=='__main__':
 #     model = Model(num_class=4)
 #     inputs = torch.randn(1, 3, 600, 600)
 #     print(model(inputs).size())
